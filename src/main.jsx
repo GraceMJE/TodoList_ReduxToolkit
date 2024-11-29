fdad8b1192d45b5f93c866944d2c469fccdbdb61
GraceMJE <akwndms123@naver.com>
2024-11-29 14:22:35 +0900

    store생성 및 main과 연결(provider설정)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import store from './redux/store.jsx'
+import { Provider } from 'react-redux';
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
